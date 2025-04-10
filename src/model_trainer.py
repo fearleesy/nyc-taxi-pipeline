cc1d61e63c1e3529ffcc779f9b5336ca712983e3
Lollilops <nikigor2004@mail.ru>
2025-04-10 20:46:06 +0300

    grid add

--- src/model_trainer.py
+++ src/model_trainer.py
@@ -1,9 +1,11 @@
+from sklearn.model_selection import GridSearchCV
 import joblib
-from sklearn.linear_model import SGDRegressor
+from sklearn.linear_model import Lasso, LinearRegression, SGDRegressor
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 import pandas as pd
+import numpy as np
 
 class TaxiModel:
     def __init__(self):
@@ -23,7 +25,7 @@ class TaxiModel:
         
         return Pipeline([
             ('preprocessor', preprocessor),
-            ('regressor', SGDRegressor())
+            ('model', Lasso())
         ])
     
     def train(self, X: pd.DataFrame, y: pd.Series):
@@ -34,6 +36,44 @@ class TaxiModel:
     
     def save(self, path: str):
         joblib.dump(self.pipeline, path)
+
+    
+    def grid(self, X_train, y_train):
+        print("\nПодбор гиперпараметров:")
+        param_grids = {
+            "Lasso" : {
+                'model__alpha': np.logspace(-4, 4, 25),          
+                'model__max_iter': range(1000, 10000, 1000)       
+            },
+            "Ridge" : {
+                'alpha': np.logspace(-4, 4, 5),
+                'solver': ['auto', 'svd', 'cholesky', 'lsqr'], 
+            },
+            'KNeighborsRegressor': {
+                'model__n_neighbors': [3, 5, 7, 9],
+                'model__weights': ['uniform', 'distance'],
+                'model__p': [1, 2]
+            },
+            'DecisionTreeRegressor': {
+                'model__max_depth': [None, 3, 5, 7, 10],
+                'model__min_samples_split': [2, 5, 10],
+                'model__min_samples_leaf': [1, 2, 4]
+            },
+            'RandomForestRegressor': {
+                'model__n_estimators': [50, 100, 200], 
+                'model__max_depth': [None, 5, 10],
+                'model__min_samples_split': [2, 5],
+                'model__bootstrap': [True, False] 
+            }
+        }
+        grid_search = GridSearchCV(estimator=self.pipeline, param_grid=param_grids["Lasso"], cv=5, scoring='neg_mean_absolute_error', n_jobs=-1)
+        grid_search.fit(X_train, y_train)
+        print(f"Модель: {self.pipeline}")
+        print(f"Лучшие параметры: {grid_search.best_params_}")
+        print(f"Лучший MAE: {(-grid_search.best_score_):.2f}")
+
+        return grid_search
+
     
     @classmethod
     def load(cls, path: str) -> 'TaxiModel':
