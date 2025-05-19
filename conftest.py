22374f9ef17dad11c70356f20d9bf2cf05e79b05
Lollilops <nikigor2004@mail.ru>
2025-05-19 12:09:50 +0300

    add tests

--- conftest.py
+++ conftest.py
@@ -3,3 +3,11 @@ import pytest
 @pytest.fixture
 def model_name():
     return "LR"
+
+@pytest.fixture
+def file_path():
+    return "train.csv"
+
+@pytest.fixture
+def file_path():
+    return "train.csv"
