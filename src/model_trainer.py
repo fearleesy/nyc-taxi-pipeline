11d702f78dc6c809df4a38acf3e8ddb3ab3a13c3
Lollilops <nikigor2004@mail.ru>
2025-05-19 11:05:12 +0300

    new preproc

--- src/model_trainer.py
+++ src/model_trainer.py
@@ -10,27 +10,62 @@ from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 import pandas as pd
 import numpy as np
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import MinMaxScaler, RobustScaler
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 import matplotlib as plt
 
 
 class TaxiModel:
     def __init__(self, model_name):
-        self.pipeline = self._build_pipeline(model_name)
-    
-    def _build_pipeline(self, model_name):
+        numeric_preprocessors = {
+            'impute_median_scaler': Pipeline([
+                ('imputer', SimpleImputer(strategy='median')),
+                ('scaler', StandardScaler())
+            ]),
+            'impute_mean_robust': Pipeline([
+                ('imputer', SimpleImputer(strategy='mean')),
+                ('scaler', RobustScaler())
+            ]),
+            'impute_zero_minmax': Pipeline([
+                ('imputer', SimpleImputer(strategy='constant', fill_value=0)),
+                ('scaler', MinMaxScaler())
+            ])
+        }
+
+        # Варианты для категориальных признаков
+        categorical_preprocessors = {
+            'onehot': OneHotEncoder(handle_unknown='ignore', sparse_output=False),
+            'onehot_drop': OneHotEncoder(handle_unknown='ignore', sparse_output=False, drop='first')
+        }
+
         numeric_features = ['pickup_longitude', 
                           'pickup_latitude', 'dropoff_longitude',
                           'dropoff_latitude']
         categorical_features = ['passenger_count', 'vendor_id']
         
-        preprocessor = ColumnTransformer(
-            transformers=[
-                ('num', StandardScaler(), numeric_features),
-                ('cat', OneHotEncoder(), categorical_features)
-            ])
+        # preprocessor = ColumnTransformer(
+        #     transformers=[
+        #         ('num', StandardScaler(), numeric_features),
+        #         ('cat', OneHotEncoder(), categorical_features)
+        #     ])
         
-        models = {
+        self.preprocessor_options = {
+            'option1': ColumnTransformer([
+                ('num', numeric_preprocessors['impute_median_scaler'], numeric_features),
+                ('cat', categorical_preprocessors['onehot'], categorical_features)
+            ]),
+            'option2': ColumnTransformer([
+                ('num', numeric_preprocessors['impute_mean_robust'], numeric_features),
+                ('cat', categorical_preprocessors['onehot_drop'], categorical_features)
+            ]),
+            'option3': ColumnTransformer([
+                ('num', numeric_preprocessors['impute_zero_minmax'], numeric_features),
+                ('cat', categorical_preprocessors['onehot'], categorical_features)
+            ])
+        }
+
+        self.models = {
             "LR" : LinearRegression(),
             "KNN" : KNeighborsRegressor(),
             "DT" : DecisionTreeRegressor(),
@@ -38,13 +73,9 @@ class TaxiModel:
             "Lasso" : Lasso(),
             "Ridge" : Ridge()
         }
-        if model_name in models:
-            return Pipeline([
-                ('preprocessor', preprocessor),
-                ('model', models[model_name])
-            ])
-        # print("Неправильное имя модели")
-        raise Exception("Неправильное имя модели")
+        if model_name not in self.models:
+            # print("Неправильное имя модели")
+            raise Exception("Неправильное имя модели")
         # return None
     
     # def train(self, X: pd.DataFrame, y: pd.Series, is_warm_start: bool):
@@ -133,22 +164,33 @@ class TaxiModel:
             # feature_names = np.concatenate([['Age', 'SibSp', 'Parch', 'Fare'], cat_features])
             # plot_coefficients(lr, feature_names)
         else:
-            grid_search = GridSearchCV(estimator=self.pipeline, param_grid=param_grids[model_name], cv=5, scoring='neg_mean_absolute_error', n_jobs=-1)
-            if is_warm_start:
-                best_n_estimators = grid_search.best_params_['n_estimators']
-                best_model = grid_search.best_estimator_
-                if best_n_estimators > old_n_estimators:
-                    print(f"\nДообучение модели: добавляем {best_n_estimators - old_n_estimators} деревьев")
-                    best_model.n_estimators = best_n_estimators
-                    best_model.fit(X_train, y_train)
+            results = []
+            for prep_name, preprocessor in self.preprocessor_options.items():
+                self.pipeline = Pipeline([
+                    ('preprocessor', preprocessor),
+                    ('model', self.models[model_name])
+            ])
+                grid_search = GridSearchCV(estimator=self.pipeline, param_grid=param_grids[model_name], cv=5, scoring='neg_mean_absolute_error', n_jobs=-1)
+                if is_warm_start:
+                    best_n_estimators = grid_search.best_params_['n_estimators']
+                    best_model = grid_search.best_estimator_
+                    if best_n_estimators > old_n_estimators:
+                        print(f"\nДообучение модели: добавляем {best_n_estimators - old_n_estimators} деревьев")
+                        best_model.n_estimators = best_n_estimators
+                        best_model.fit(X_train, y_train)
+                    else:
+                        print("\nЛучшая модель уже оптимальна (n_estimators не увеличилось)")
                 else:
-                    print("\nЛучшая модель уже оптимальна (n_estimators не увеличилось)")
-            else:
-                grid_search.fit(X_train, y_train)
+                    grid_search.fit(X_train, y_train)
+                results.append({
+                    'Model': model_name,
+                    'Preprocessor': prep_name,
+                    'Best Score': grid_search.best_score_,
+                    'Best Params': grid_search.best_params_
+                })
 
-            print(f"Модель: {model_name}")
-            print(f"Лучшие параметры: {grid_search.best_params_}")
-            print(f"Лучший MAE: {(-grid_search.best_score_):.2f}")
+            results_df = pd.DataFrame(results).sort_values('Best Score', ascending=False)
+            print(results_df.head())
 
             self.pipeline = best_model if is_warm_start else grid_search
 
